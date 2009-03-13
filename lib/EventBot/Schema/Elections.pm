1d109f3a187ff35ebbf3ada28fe8811bbaeefbfe
Toby Corkindale <tjc@cpan.org>
2009-03-13 17:14:56 +1100

    Add support for starting and voting in elections.

--- lib/EventBot/Schema/Elections.pm
+++ lib/EventBot/Schema/Elections.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use parent 'DBIx::Class';
 
-__PACKAGE__->load_components(qw(InflateColumn::DateTime Core));
+__PACKAGE__->load_components(qw(ResultSetManager InflateColumn::DateTime Core));
 __PACKAGE__->table('elections');
 __PACKAGE__->add_columns(
     id => { data_type => "INTEGER", is_nullable => 0, is_auto_increment => 1 },
@@ -47,6 +47,119 @@ sub candidates {
     return map {
         $self->result_source->schema->resultset('Pubs')->find($_)
     } split(':', $self->candidate_list);
+}
+
+=head2 vote
+
+Usage:
+
+  $user = $schema->resultset('People')->find(...);
+  $election->vote('A', $user);
+
+=cut
+
+sub vote {
+    my ($self, $vote, $person) = @_;
+    die("Missing vote") unless $vote;
+    die("Missing person") unless $person;
+
+    die("Invalid vote: $vote") unless $vote =~ /^[A-Z]$/;
+
+    # Need to convert $pub into the correct ID from the candidates..
+    my $vote_num = ord($vote) - ord('A');
+    my @cand_list = split(':', $self->candidate_list);
+    my $candidate_id = $cand_list[$vote_num];
+    my $pub = $self->result_source->schema->resultset('Pubs')
+        ->find($candidate_id);
+
+    die("Invalid vote, pub $vote ($candidate_id) not found") unless $pub;
+
+    $self->result_source->schema->txn_do(sub {
+        $self->search_related('votes',
+            {
+                person => $person->id
+            }
+        )->delete;
+        $self->add_to_votes(
+            {
+                person => $person,
+                pub => $pub
+            }
+        );
+    });
+}
+
+=head2 commence
+
+Start a new election!
+
+This determines the candidates, and will return the election object.
+
+=cut
+
+sub commence :ResultSet {
+    my ($self) = @_;
+    my $max_id_query = $self->result_source->schema->storage->dbh->prepare(
+        'SELECT MAX(id) FROM pubs'
+    );
+    $max_id_query->execute;
+    my ($max_id) = $max_id_query->fetchrow_array;
+    warn "Maximum ID of pubs: $max_id\n";
+    die("Apparently no pubs?") unless $max_id > 5; # sanity check.
+
+    my $election = $self->create(
+        {
+            enabled => 1
+        }
+    );
+
+    my $none_of_the_above = $self->result_source->schema->resultset('Pubs')
+        ->find_or_create(
+            {
+                name => 'None of the Above',
+                region => 'Anywhere',
+                street_address => 'To be specified manually'
+            }
+        );
+
+    my @pubs;
+    while (@pubs < 4) {
+        my $pub = $self->_get_random_pub($max_id);
+        next if $pub ~~ \@pubs; # Avoid dups! ;)
+        next unless $pub->endorsed;
+        push(@pubs, $pub);
+    }
+    push(@pubs, $none_of_the_above);
+    $election->candidates(@pubs);
+
+    return $election;
+}
+
+=head2 _get_random_pub
+
+This finds us a random pub out of the whole set, given the maximum ID.
+
+Note that it's not very efficient.. We'd be better off doing and initial
+count() of available pubs, then:
+
+SELECT id FROM pubs
+WHERE endorsed IS TRUE
+OFFSET $random
+LIMIT 1
+
+But eh, it works anyway..
+
+=cut
+
+sub _get_random_pub :ResultSet {
+    my ($self, $max) = @_;
+
+    my $pub;
+    do {
+        my $id = int(rand($max+1));
+        $pub = $self->result_source->schema->resultset('Pubs')->find($id);
+    } while (not $pub);
+    return $pub;
 }
 
 1;
