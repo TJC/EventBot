e3c3bb29925a80cee2bca9e4d79a815c6623ca7f
Toby Corkindale <tjc@wintrmute.net>
2009-06-23 16:11:30 +1000

    Add initial event attendence code. Currently is simplified version of what exists in EventBot.pm

--- lib/EventBot/MailParser/Events.pm
+++ lib/EventBot/MailParser/Events.pm
@@ -3,7 +3,40 @@ use strict;
 use warnings;
 
 sub parse {
-    return;
+    my ($class, $subject, $body) = @_;
+    my $event_id;
+
+    if ($subject =~ /\[event (\d+)\]/i) {
+        $event_id = $1;
+        warn "Suspected event ID: $event_id\n";
+    }
+    my @commands;
+    my @lines = split("\n", $body);
+    # TODO: Bring over the event detection routines
+
+    for my $line (@lines) {
+        if (my($mode, $name) = $line =~
+              /^\s*
+              ([-\+\?])
+              \s*
+              [`']?
+              ([\w\d][[:print:]]+?)
+              [`']?
+              \s*$
+              /x
+          ) {
+              push(@commands, {
+                      type => 'attend',
+                      data => {
+                          name => $name,
+                          mode => $mode,
+                          event => $event_id,
+                      }
+                  }
+              );
+        }
+    }
+    return \@commands;
 }
 
 1;
