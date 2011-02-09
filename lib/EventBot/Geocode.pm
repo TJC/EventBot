60533e8f1d3c3de8699f6907f1861d6ba5c0b1b1
Toby Corkindale <tjc@wintrmute.net>
2011-02-09 17:45:44 +1100

    Geocoding scripts for EventBot

---
+++ lib/EventBot/Geocode.pm
@@ -0,0 +1,42 @@
+package EventBot::Geocode;
+use Moose;
+use LWP::UserAgent;
+use URI::Escape qw(uri_escape);
+use JSON;
+
+has 'geocode_url' => (
+    is => 'rw',
+    isa => 'Str',
+    default => 'http://maps.googleapis.com/maps/api/geocode/json',
+);
+
+has 'region' => (
+    is => 'rw',
+    isa => 'Str',
+    default => 'uk',
+);
+
+sub address {
+    my ($self, $address) = @_;
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout(30);
+    my $params = join('&',
+        "address=" . uri_escape($address),
+        'sensor=false',
+        'region=' . $self->region
+    );
+    my $url = join('?', $self->geocode_url, $params);
+    warn "URL = $url\n";
+    my $response = $ua->get($url);
+
+    my $json = JSON->new;
+    my $result = $json->decode($response->content);
+
+    die("Failed: " . $result->{status} . "\n")
+        unless ($result->{status} eq 'OK');
+
+    return $result->{results}->[0]->{geometry}->{location};
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
