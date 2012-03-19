d9426344c18abcd8ea6cde2a2ccd11d19d5796bb
Toby Corkindale <tjc@wintrmute.net>
2012-03-19 20:41:31 +1100

    Add support for editing endorsements via Admin keys

--- lib/EventBot/WWW/C/Manage/Pub.pm
+++ lib/EventBot/WWW/C/Manage/Pub.pm
@@ -21,7 +21,75 @@ has 'form' => (
 
 sub endorse : Chained('/pub/pub_name') PathPart Args(0) {
     my ($self, $c) = @_;
+    $c->stash->{template} = 'manage/pub/endorse.tt';
 
+    # I really, really need to fix the table to identify "real" users
+    # another way.. So start with potential users..
+    my @pot_users = $c->model('DB::People')->search(
+        {},
+        # { \[ 'length(email) <> 32' ] },
+        { order_by => 'name' },
+    );
+    $c->stash->{all_users} = [
+        grep { $_->email !~ /^[0-9a-f]{32}$/ } @pot_users
+    ];
+
+    $c->stash->{all_admins} = $c->model('DB::Admins')->search(
+        { active => 1 },
+        { order_by => 'name' },
+    );
+
+    # Figure out current endorsers:
+    $c->stash->{current_endorsers} = [
+        map { $_->id } $c->stash->{pub}->nominees
+    ];
+
+    return unless ($c->request->method =~ /post/i
+        and $c->request->params->{admin} =~ /^\d+$/
+    );
+
+    my $admin = $c->model('DB::Admins')->search(
+        {
+            id => $c->request->params->{admin},
+            password => $c->request->params->{pw}
+        }
+    )->next;
+
+    unless ($admin) {
+        $c->stash->{message} = 'Incorrect admin password';
+        return;
+    }
+
+    $c->model('DB')->schema->txn_do(sub {
+        $admin->create_related('logs', {
+            action => "Adjusted endorsements on pub: "
+                . $c->stash->{pub}->name
+        });
+
+        $c->stash->{pub}->search_related('endorsements')->delete;
+
+        for my $i (1..2) {
+            my $eid = $c->request->params->{"endorser$i"};
+            next if ($eid eq 'DELETE');
+            next unless ($eid =~ /^\d+$/);
+            $c->stash->{pub}->create_related('endorsements',
+                {
+                    pub => $c->stash->{pub}->id,
+                    person => $eid,
+                }
+            );
+        }
+
+        my $e_count = $c->model('DB::Endorsements')->search(
+            { pub => $c->stash->{pub}->id }
+        )->count;
+
+        # Require at least two endorsements to be endorsed:
+        $c->stash->{pub}->endorsed( $e_count >= 2 ? 1 : 0 );
+        $c->stash->{pub}->update;
+    });
+
+    $c->stash->{message} = 'Saved!';
 }
 
 sub create : Path('/manage/pub/create') {
